308cd61a004df657f2982948c35cf5f5ea05ec9d
Raman Kryvetski <romik.krivetskiy@gmail.com>
2023-10-18 17:59:52 +0200

    feat: update url backend

--- frontend/src/components/Student.js
+++ frontend/src/components/Student.js
@@ -23,7 +23,7 @@ export default function Student() {
     e.preventDefault()
     const student = { name, address }
     console.log(student)
-    fetch("http://backend:8080/student/add", {
+    fetch("http://localhost:8080/student/add", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(student)
@@ -34,7 +34,7 @@ export default function Student() {
   }
 
   useEffect(() => {
-    fetch("http://backend:8080/student/getAll")
+    fetch("http://localhost:8080/student/getAll")
       .then(res => res.json())
       .then((result) => {
         setStudents(result);
